780109503d4b366090f37a09600ebbd86ac7d65a
PatrykFrackowiak <patryk.frackowiak90@gmail.com>
2020-08-28 15:59:00 +0200

    Ready for 15.4

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,32 +3,58 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column';
+import { settings } from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
+import Creator from '../Creator/Creator';
 
 class List extends React.Component {
+    state = {
+        columns: this.props.columns || [],
+    }
     static propTypes = {
         title: PropTypes.node.isRequired,
-        imageSrc: PropTypes.string,
-        children: PropTypes.node,
+        image: PropTypes.string,
+        description: PropTypes.node,
+        columns: PropTypes.array,
     }
     static defaultProps = {
-        children: <p>I can do all the things!!!</p>,
+        description: settings.defaultListDescription,
     }
 
     render() {
         return (
             <section className={styles.component}>
-                <Hero titleText={this.props.title} imageSrc={this.props.imageSrc} />
+                <Hero titleText={this.props.title} imageSrc={this.props.image} />
                 <div className={styles.description}>
-                    {this.props.children}
+                    {ReactHtmlParser(this.props.description)}
                 </div>
+`                <div className={styles.creator}>
+                    <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
+                </div>`
                 <div className={styles.columns}>
-                    <Column title='Id'/>
-                    <Column title='Name'/>
-                    <Column title='Details'/>
+                    {this.state.columns.map(({ key, ...columnProps }) => (
+                        <Column key={key} {...columnProps} />
+                    ))}
                 </div>
             </section>
         )
     }
+
+    addColumn(title){
+        this.setState(state => (
+          {
+            columns: [
+              ...state.columns,
+              {
+                key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
+                title,
+                icon: 'list-alt',
+                cards: []
+              }
+            ]
+          }
+        ));
+      }
 }
 
 export default List;
