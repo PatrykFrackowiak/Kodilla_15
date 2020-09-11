ab6ec2e10eb8a408c47371088bb60ab48c64a97c
PatrykFrackowiak <patryk.frackowiak90@gmail.com>
2020-09-11 20:45:54 +0200

    16.4 Added add functions

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,18 +2,24 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card';
-// import Creator from '../Creator/Creator';
+import Creator from '../Creator/Creator';
 import Icon from '../Icon/Icon';
+import { settings } from '../../data/dataStore';
 
 class Column extends React.Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
     cards: PropTypes.array,
+    addCard: PropTypes.func,
+  };
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
   };
 
   render() {
-    const { title, icon, cards } = this.props;
+    const { title, icon, cards, addCard } = this.props;
     return (
       <div className={styles.component}>
         <h3 className={styles.title}>
@@ -26,10 +32,7 @@ class Column extends React.Component {
           {cards.map(({ id, title }) => (
             <Card key={id} title={title} />
           ))}
-          {/*  <Creator
-            text={settings.cardCreatorText}
-            action={(title) => this.addCard(title)}
-          /> */}
+          <Creator text={settings.cardCreatorText} action={addCard} />
         </div>
       </div>
     );
---
+++ src/redux/cardsRedux.js
@@ -0,0 +1,26 @@
+import shortid from 'shortid';
+
+// selectors
+export const getCardsForColumn = ({ cards }, columnId) =>
+  cards.filter((card) => card.columnId == columnId);
+
+// action name creator
+const reducerName = 'cards';
+const createActionName = (name) => `app/${reducerName}/${name}`;
+
+// action types
+export const ADD_CARD = createActionName('ADD_CARD');
+
+export const createActionAddCard = (payload) => ({
+  payload: { ...payload, id: shortid.generate() },
+  type: ADD_CARD,
+});
+
+export default function reducer(state = [], action = {}) {
+  switch (action.type) {
+    case ADD_CARD:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+}
